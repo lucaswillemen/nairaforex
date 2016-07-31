bf9eedf383a53ef1dbed08b963fbe23a0c87f7cb
Lucas Willemen <lucaswillemen@msn.com>
2016-07-31 19:50:45 -0300

    DB data

--- application/config/config.php
+++ application/config/config.php
@@ -16,7 +16,7 @@
 | environments.
 |
 */
-$config['base_url'] = 'http://www.valecooper.com.br/backoffice/';
+$config['base_url'] = 'http://localhost/nairaforex';
 
 /*
 |--------------------------------------------------------------------------
--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = '';
+$db['default']['password'] = 'Cpdnf@2016';
 $db['default']['database'] = 'nairaforex';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
