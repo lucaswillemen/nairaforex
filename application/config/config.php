bbda8bb780818bd4c3765d10badc5a84b08a44d9
Lucas Willemen <lucaswillemen@msn.com>
2016-07-31 19:57:11 -0300

    install production

--- application/config/config.php
+++ application/config/config.php
@@ -16,7 +16,7 @@
 | environments.
 |
 */
-$config['base_url'] = 'http://www.nairaforex/backoffice';
+$config['base_url'] = '';
 
 /*
 |--------------------------------------------------------------------------
